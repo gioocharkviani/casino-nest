4499145741b6fabbade6a64a53edcebaac021086
gioocharkviani <gioocharkviani@gmail.com>
2024-10-09 10:44:31 +0400

    remove gitignor

--- src/main.ts
+++ src/main.ts
@@ -6,13 +6,13 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // Enable CORS
   app.enableCors({
-    origin: "http://localhost:3000",
+    origin: "*",
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
     credentials: true,
   });
 
   app.useGlobalPipes(new ValidationPipe());
   app.setGlobalPrefix("api");
-  await app.listen(3001);
+  await app.listen(6545);
 }
 bootstrap();
